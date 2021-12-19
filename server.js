6e4b4532bf733ed092b5cae98b49e7e94b86863f
monicatemburu <saimonicasowmyasri@gmail.com>
2021-12-19 14:20:45 +0530

    commit last

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/myportfolio'));
+// app.use(express.static('./dist/myportfolio'));
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/myportfolio/'}),
-);
+app.get('/*', (req, res) =>{
+    // res.sendFile('index.html', {root: 'dist/myportfolio/'}),
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
