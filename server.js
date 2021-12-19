1cb1acc6cb779199540d5952dddfa3d8853b1f6d
monicatemburu <saimonicasowmyasri@gmail.com>
2021-12-19 15:30:31 +0530

    commit last

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/portfolio'));
 app.listen(process.env.PORT || 8080);
 const path = require('path');
 app.get('/*', function(req, res){
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/portfolio/index.html'));
 });
 console.log('Server listening on port 8080');
