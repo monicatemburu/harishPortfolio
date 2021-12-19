fb58737fd4419ebfb4f4220b30a54e15e958938c
monicatemburu <saimonicasowmyasri@gmail.com>
2021-12-19 14:58:01 +0530

    commit last

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname));
 
 app.get('/*', (req, res) =>{
     // res.sendFile('index.html', {root: 'dist/myportfolio/'}),
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname ));
 });
 
 // Start the app by listening on the default Heroku port
